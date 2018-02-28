0eea60de32cb2182df1ac9e19edfc4bea02b891a
chenglongxie <chenglongxie@outlook.com>
2018-02-28 10:04:09 +0800

    init
    基于kong-oidc 1.0.4版本修改，增加X-AccessToken的header

---
+++ kong/plugins/oidc/schema.lua
@@ -0,0 +1,17 @@
+return {
+  no_consumer = true,
+  fields = {
+    client_id = { type = "string", required = true },
+    client_secret = { type = "string", required = true },
+    discovery = { type = "string", required = true, default = "https://.well-known/openid-configuration" },
+    introspection_endpoint = { type = "string", required = false },
+    redirect_uri_path = { type = "string" },
+    scope = { type = "string", required = true, default = "openid" },
+    response_type = { type = "string", required = true, default = "code" },
+    ssl_verify = { type = "string", required = true, default = "no" },
+    token_endpoint_auth_method = { type = "string", required = true, default = "client_secret_post" },
+    session_secret = { type = "string", required = false },
+    recovery_page_path = { type = "string" },
+    filters = { type = "string" }
+  }
+}
